9e170af8a41b385b81715de3b449b8741b59aa02
Macbook Pro <joserodriguez@Joses-MacBook-Pro.local>
2022-02-03 19:10:33 -0800

    modifications

--- static/script.js
+++ static/script.js
@@ -62,13 +62,6 @@ function drop(ev) {
   var source = document.querySelector(`#${data}`).getAttribute('src');
   console.log(source);
 
-  // target is on the html where you have drop
-  // var myElement = document.getElementById(data);
-  var myNewElement = document.createElement('img');
-  myNewElement.src = source;
-  console.log(myNewElement)
-  // const fileList = ev.dataTransfer.files;
-
   ev.target.appendChild(myNewElement);
 
   ev.dataTransfer.clearData();
@@ -163,3 +156,38 @@ function updateThumbnail(dropZoneElement, file) {
 //         });
 //         reader.readAsDataURL(this.files[0]);
 // });
+
+
+// Try drag and drop implementation
+let url = 'https://cdn.shopify.com/s/files/1/0234/8017/2591/products/young-man-in-bright-fashion_925x_f7029e2b-80f0-4a40-a87b-834b9a283c39.jpg'
+const toDataURL = url => fetch(url)
+      .then(response => response.blob())
+      .then(blob => new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onloadend = () => resolve(reader.result)
+      reader.onerror = reject
+      reader.readAsDataURL(blob)
+     }))
+
+
+// ***Here is code for converting "Base64" to javascript "File Object".***
+
+  function dataURLtoFile(dataurl, filename) {
+     var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+     bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+     while(n--){
+     u8arr[n] = bstr.charCodeAt(n);
+     }
+   return new File([u8arr], filename, {type:mime});
+  }
+
+
+// *** Calling both function ***
+
+  toDataURL(url)
+  .then(dataUrl => {
+     console.log('Here is Base64 Url', dataUrl)
+     var fileData = dataURLtoFile(dataUrl, "imageName.jpg");
+     console.log("Here is JavaScript File Object",fileData)
+     fileArr.push(fileData)
+   })
